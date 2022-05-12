7bfa3054db2547d0da2af61bba3ba181657a84cc
Alex Kokurin <76799174+AlexKokurin@users.noreply.github.com>
2022-05-12 20:10:39 +0300

    Add files via upload
    создание мяча

--- ping pong.py
+++ ping pong.py
@@ -4,7 +4,7 @@ from pygame import *
 from random import randint
 
 class GameSprite(sprite.Sprite):
-    def __init__(self,player_image1,x,y,speed,e,e1,pp):
+    def __init__(self,player_image1,x,y,speed,e,e1):
         super().__init__()
         self.image = transform.scale(image.load(player_image1),(e,e1))
         self.rect = self.image.get_rect()
@@ -12,13 +12,18 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
         self.speed = speed
         self.f = 0
-        self.pp = pp
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
+
+class Bal(GameSprite):
+    pass
 
 window = display.set_mode((800,600))
 display.set_caption('Ping pong')
 background = transform.scale(image.load('background.jpg'),(800,600))
+
+bal = Bal('p_p.png',150,150,2,90,90)
+
 game = True
 finish = False
 clock = time.Clock()
@@ -30,4 +35,5 @@ while game:
             game = False
     if not finish:
         window.blit(background,(0,0))
+        bal.reset()
     display.update()
